fb8d7082a0ef99d85d03cbd3d5834e16561125a8
pramodya1994 <bpmmendis94@gmail.com>
2019-11-26 11:35:31 +0530

    Add dialog box in ParentComponent

--- src/Project-Components/ParentComponent.jsx
+++ src/Project-Components/ParentComponent.jsx
@@ -4,60 +4,131 @@ import React, {Component} from "react";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import Avatar from "@material-ui/core/Avatar";
-import CardContent from "@material-ui/core/CardContent";
 import PropTypes from 'prop-types';
+import AddIcon from '@material-ui/icons/Add';
+import {makeStyles} from '@material-ui/core/styles';
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import TextField from "@material-ui/core/TextField";
+import DialogActions from "@material-ui/core/DialogActions";
+import Button from "@material-ui/core/Button";
+import CardActionArea from "@material-ui/core/CardActionArea";
 
 const styles = {
     card: {
         maxWidth: 245,
-    },
-    media: {
-        height: 0,
-        paddingTop: '56.25%',
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
+        borderStyle: 'solid',
+        borderWidth: '1px',
+        borderColor: '#bfbfbf',
     },
     avatar: {
         backgroundColor: 'blue'
     },
     cardContent: {
-        backgroundColor: '#d9d9d9',
+        backgroundColor: '#f5f5f5',
         padding: '5px 5px 5px 5px',
+        borderTopStyle: 'solid',
+        borderTopWidth: '1px',
+        borderTopColor: '#bfbfbf',
+    },
+    parentDiv: {
+        display: 'flex',
+        width: 290,
+    },
+    cardDiv: {
+        width: '80%',
+    },
+    buttonDiv: {
+        flexGrow: '1',
     }
 };
 
 
-export default class ParentComponent extends Component {
+class ParentComponent extends Component {
 
-    render(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+        };
+    }
+
+    handleClickOpen = () => {
+        this.setState({
+            open: true,
+        })
+    };
+
+    handleClose = () => {
+        this.setState({
+            open: false,
+        })
+    };
+
+    render() {
         return (
             <div>
-                <Card style={styles.card}>
-                    <CardHeader
-                        avatar={
-                            <Avatar aria-label="recipe" style={styles.avatar}>
-                                {this.props.avatar}
-                            </Avatar>
-                        }
-                        action={
-                            <IconButton aria-label="settings">
-                                <MoreVertIcon />
-                            </IconButton>
-                        }
-                        title={this.props.title}
-                        subheader={this.props.subheader}
-                    />
-                    <CardContent style={styles.cardContent}>
-                        + Payload<br/>
-                        + Attributes
-                    </CardContent>
-                </Card>
+                <div style={styles.parentDiv}>
+                    <div style={styles.cardDiv}>
+                        <Card style={styles.card}>
+                            <CardActionArea onClick={this.handleClickOpen}>
+                                <CardHeader
+                                    avatar={
+                                        <Avatar aria-label="recipe" style={styles.avatar}>
+                                            {this.props.avatar}
+                                        </Avatar>
+                                    }
+                                    action={
+                                        <IconButton aria-label="settings">
+                                            <MoreVertIcon/>
+                                        </IconButton>
+                                    }
+                                    title={this.props.title}
+                                    subheader={this.props.subheader}
+                                />
+                            </CardActionArea>
+                        </Card>
+                    </div>
+                    <div style={styles.buttonDiv}>
+                        <IconButton aria-label="settings">
+                            <AddIcon/>
+                        </IconButton>
+                    </div>
+                </div>
+                <Dialog open={this.state.open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            To subscribe to this website, please enter your email address here. We will send updates
+                            occasionally.
+                        </DialogContentText>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Email Address"
+                            type="email"
+                            fullWidth
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Cancel
+                        </Button>
+                        <Button onClick={this.handleClose} color="primary">
+                            Subscribe
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </div>
         );
     }
 
 }
+
+export default ParentComponent;
 
 
 ParentComponent.propTypes = {
